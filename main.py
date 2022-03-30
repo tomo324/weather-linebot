fe41f48dff13c69c0f5a2a0b1ac37179087c746c
tomo324 <0204tomo.mc@gmail.com>
2022-03-31 00:43:18 +0900

    20220331

--- main.py
+++ main.py
@@ -2,15 +2,22 @@ import json
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
 
+#この一行を追加した
+import weather_search
+
+
 file = open('info.json','r')
 info = json.load(file)
 
 CHANNEL_ACCESS_TOKEN = info['CHANNEL_ACCESS_TOKEN']
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
+#この一行も追加
+umbrella = weather_search.rain_judge()
+
 def main():
     USER_ID = info['USER_ID']
-    messages = TextSendMessage(text="傘を持っていきましょう！")
+    messages = TextSendMessage(text = umbrella)
     line_bot_api.push_message(USER_ID,messages=messages)
 
 if __name__ == "__main__":
--- weather_search.py
+++ weather_search.py
@@ -1,15 +1,12 @@
-import requests
-res = requests.get('https://weather.tsukumijima.net/api/forecast?city=130010')
-res_json = res.json()
-forecasts = res_json['forecasts'][0]
-info = forecasts['dateLabel'] + ':' + forecasts['detail']['weather']
-weather = info.find('雨')
+def rain_judge():
+    from operator import truediv
+    import requests
+    res = requests.get('https://weather.tsukumijima.net/api/forecast?city=130010')
+    res_json = res.json()
+    forecasts = res_json['forecasts'][0]
+    info = forecasts['dateLabel'] + ':' + forecasts['detail']['weather']
+    weather = info.find('雨')
+    if weather >= 0:
+        return '傘を持っていきましょう!'
 
-#雨予報か判定
-if weather > 0:
-    print('傘を持っていきましょう！')
-else:
-    print(None)
-
-print(info)
-print(weather)
+print(rain_judge())
