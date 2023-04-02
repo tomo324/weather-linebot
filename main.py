02c307b71a0bc6e44f31cf49d08d53a4b6b4af00
tomo324 <0204tomo.mc@gmail.com>
2023-04-02 17:10:43 +0900

    add get

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from linebot.models import TextSendMessage
 import weather_search 
 
 
-CHANNEL_ACCESS_TOKEN = os.environ(['CHANNEL_ACCESS_TOKEN'])
+CHANNEL_ACCESS_TOKEN = os.environ.get('CHANNEL_ACCESS_TOKEN')
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
 umbrella = weather_search.rain_judge()
 
 def main():
-    USER_ID = os.environ(['USER_ID'])
+    USER_ID = os.environ.get('USER_ID')
     messages = TextSendMessage(text = umbrella[0]+ '\n' + '----------------------' + '\n' +  umbrella[1])
     line_bot_api.push_message(USER_ID,messages=messages)
 
